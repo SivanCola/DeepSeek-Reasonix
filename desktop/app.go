429f05bb1e1df362ebac75585e0d3fba00797a0a
Sivan <liuxw96@foxmail.com>
2026-06-05 23:39:36 +0800

    Fix desktop model recovery across tabs

--- desktop/app.go
+++ desktop/app.go
@@ -2299,6 +2299,10 @@ func (a *App) SetModel(name string) error {
 }
 
 func (a *App) SetModelForTab(tabID, name string) error {
+	return a.setModelForTab(tabID, name, false)
+}
+
+func (a *App) setModelForTab(tabID, name string, force bool) error {
 	if a.ctx == nil || name == "" {
 		return nil
 	}
@@ -2306,7 +2310,7 @@ func (a *App) SetModelForTab(tabID, name string) error {
 	if tab == nil {
 		return nil
 	}
-	if name == tab.model {
+	if name == tab.model && !force {
 		return nil
 	}
 	if tab.Ctrl != nil && tab.Ctrl.Running() {
